2bc049238364abc6ead6540fc69c255e4e20af09
jikim-research <wknds2208@gmail.com>
2026-01-05 14:07:52 +0900

    reimplemented paper performance (table 5,6)

--- scripts/test_denoisinggait.sh
+++ scripts/test_denoisinggait.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Test script for DenoisingGait on CCPG dataset
+# 체크포인트: GaitBasefusion_p0.2_threshold0.1_test_daFalse_8416-80000.pt
+# 설정 파일에서 evaluator_cfg.restore_hint: 80000으로 자동 로드됨
+
 eval "$(conda shell.bash hook)"
 conda activate opengait
 
